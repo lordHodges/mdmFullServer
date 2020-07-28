29661f460d63f2d7bc1ca1a2428367bac10d66c9
victor hodges <hodgestroncoso@gmail.com>
2020-07-28 03:42:38 -0400

    normalizacion de ramas

--- backend/mdmServer/index.js
+++ backend/mdmServer/index.js
@@ -9,7 +9,7 @@ app.set('port',process.env.PORT || 3030);
 
 //middleware
  app.use(cors({origin: 'http://localhost:4200'}));
-/*app.use(cors({origin: 'http://localhost:3030'})); */
+app.use(cors({origin: 'http://localhost:3030'})); 
 app.use(morgan('dev'));
 app.use(express.json());
 
@@ -22,5 +22,3 @@ app.use('/api/equipo', require('./routes/equipo.routes'));
 app.listen(app.get('port'),()=>{
     console.log('online in port !!', app.get('port'));
 });
-
-
